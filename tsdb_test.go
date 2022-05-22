68b0df2b56e5875c5550b93f81e01dd7911f90b7
hhy <huanghaoyuanhhy@gmail.com>
2022-05-22 11:51:52 +0800

    tsdb: improve test coverage

--- tsdb_test.go
+++ tsdb_test.go
@@ -9,8 +9,9 @@ import (
 
 func TestTSDB_WritePoints(t *testing.T) {
 	db := New[int](10 * time.Minute)
-	point := NewPoint[int]([]Tag{{Key: "cpu", Value: "#0"}}, time.Unix(100, 0), 100)
-	points := []Point[int]{point}
+	point1 := NewPoint[int]([]Tag{{Key: "cpu", Value: "#0"}}, time.Unix(100, 0), 100)
+	point2 := NewPoint[int]([]Tag{{Key: "cpu", Value: "#0"}}, time.Unix(200, 0), 100)
+	points := []Point[int]{point1, point2}
 
 	err := db.WritePoints(points)
 	assert.Nil(t, err)
@@ -19,15 +20,14 @@ func TestTSDB_WritePoints(t *testing.T) {
 	for _, p := range db.s.partitions {
 		for s, e := range p.store {
 			if s == "cpu=#0" {
-				assert.Len(t, e.values, 1)
+				assert.Len(t, e.values, 2)
 				seen = true
 			}
 		}
 	}
-
 	assert.True(t, seen)
 
-	point = NewPoint[int]([]Tag{}, time.Unix(100, 0), 100)
+	point := NewPoint[int]([]Tag{}, time.Unix(100, 0), 100)
 	points = []Point[int]{point}
 	err = db.WritePoints(points)
 	assert.ErrorIs(t, err, ErrPointMissingTag)
